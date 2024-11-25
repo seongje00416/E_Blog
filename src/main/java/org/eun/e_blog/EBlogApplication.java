30942c99ed8cf0019685a4a720a0be3d1a4cfaa0
seongje00416 <seongje00416@gmail.com>
2024-11-25 11:32:56 +0900

    Initialize Project

---
+++ src/main/java/org/eun/e_blog/EBlogApplication.java
@@ -0,0 +1,13 @@
+package org.eun.e_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBlogApplication.class, args);
+    }
+
+}
